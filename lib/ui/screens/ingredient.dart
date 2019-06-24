53a52db3b188cb0fc8cd6e0e0fc463788f41bd38
Jason Cho <jason.cho.86@gmail.com>
2019-06-23 21:26:46 -0400

    Completed Ingredient page

--- lib/ui/screens/ingredient.dart
+++ lib/ui/screens/ingredient.dart
@@ -37,6 +37,7 @@ class IngredientScreenState extends State<IngredientScreen> {
   // }
 
   final crudObj = new CrudMethods();
+  var unitVal;
 
   Widget _buildRow(ScreenArguments args) {
     // String unitType = args.unitType;
@@ -44,85 +45,130 @@ class IngredientScreenState extends State<IngredientScreen> {
         stream: Firestore.instance.collection('ingredients').snapshots(),
         builder: (context, snapshot) {
           DocumentSnapshot doc = snapshot.data.documents[args.index];
-          return Column(mainAxisAlignment: MainAxisAlignment.center, children: <
-              Widget>[
-            Text(doc['name'], style: TextStyle(fontSize: 30.0)),
-            Row(
+          return Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                IconButton(
-                    icon: Icon(Icons.arrow_back_ios),
-                    onPressed: () async {
-                      await crudObj.updateData(
-                          doc.documentID, {'unitVal': doc['unitVal'] - 1});
-                    }),
-                GestureDetector(
-                    onTap: () => Navigator.pop(context),
-                    child: Container(
-                      decoration: const BoxDecoration(color: Color(0xffddddff)),
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text(
-                          doc['unitVal'].toString() + ' ' + doc['unitType']),
-                    )),
-                IconButton(
-                    icon: Icon(Icons.arrow_forward_ios),
-                    onPressed: () async {
-                      await crudObj.updateData(
-                          doc.documentID, {'unitVal': doc['unitVal'] + 1});
-                    }),
-              ],
-            )
-          ]);
-        });
-    /* return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            args.name,
-            style: TextStyle(fontSize: 30.0),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                  icon: Icon(Icons.arrow_back_ios),
+                Padding(
+                  padding: EdgeInsets.all(80.0),
+                ),
+                Text(doc['name'], style: TextStyle(fontSize: 30.0)),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.arrow_back_ios),
+                        onPressed: () async {
+                          await crudObj.updateData(
+                              doc.documentID, {'unitVal': doc['unitVal'] - 1});
+                        }),
+                    GestureDetector(
+                        onTap: () {
+                          updateDialog(context, doc, doc['unitType']);
+                        },
+                        child: Container(
+                          decoration:
+                              const BoxDecoration(color: Colors.black12),
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                              doc['unitVal'].toString() + ' ' + doc['unitType'],
+                              style: TextStyle(fontSize: 18)),
+                        )),
+                    IconButton(
+                        icon: Icon(Icons.arrow_forward_ios),
+                        onPressed: () async {
+                          Navigator.of(context).pop();
+                          await crudObj.updateData(
+                              doc.documentID, {'unitVal': doc['unitVal'] + 1});
+                        }),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                ),
+                RaisedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      'Go Back',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    textColor: Colors.white,
+                    color: Colors.blue),
+                Padding(
+                  padding: EdgeInsets.all(70.0),
+                ),
+                RaisedButton(
                   onPressed: () async {
-                    await crudObj.updateData(args.document.documentID,
-                        {'unitVal': args.unitVal - 1});
-                  }),
-              GestureDetector(
-                  onTap: () => Navigator.pop(context),
-                  child: Container(
-                    decoration: const BoxDecoration(color: Color(0xffddddff)),
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(args.unitVal.toString() + ' ' + args.unitType),
-                  )),
-              IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: null),
+                    Navigator.of(context).pop();
+                    await crudObj.deleteData(doc.documentID);
+                  },
+                  child: Text(
+                    'Delete',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  textColor: Colors.white,
+                  color: Colors.red,
+                )
+              ]);
+        });
+  }
+
+  Future<bool> updateDialog(
+      BuildContext context, DocumentSnapshot doc, String unitType) async {
+    if (unitType == 'whole') {
+      unitType = 'quantity';
+    } else if (unitType == 'ounces' ||
+        unitType == 'grams' ||
+        unitType == 'pounds') {
+      unitType = 'weight';
+    } else {
+      unitType = 'volume';
+    }
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Update ' + unitType + '.'),
+            content: Container(
+                height: 50.0,
+                width: 150.0,
+                child: TextField(
+                  decoration: InputDecoration(hintText: 'Enter a number.'),
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    this.unitVal = int.parse(value);
+                  },
+                  onSubmitted: (value) async {
+                    Navigator.of(context).pop();
+                    if (value != 0) {
+                      await crudObj
+                          .updateData(doc.documentID, {'unitVal': value});
+                    } else {
+                      await crudObj.deleteData(doc.documentID);
+                    }
+                  },
+                )),
+            actions: <Widget>[
+              RaisedButton(
+                child: Text('Update'),
+                textColor: Colors.white,
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  if (this.unitVal != 0) {
+                    await crudObj
+                        .updateData(doc.documentID, {'unitVal': this.unitVal});
+                  } else {
+                    await crudObj.deleteData(doc.documentID);
+                  }
+                },
+              )
             ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(onPressed: () {}, child: Text('Save')),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-              ),
-              RaisedButton(onPressed: () {}, child: Text('Delete'))
-            ],
-          )
-        ]); */
-    // return ListTile(
-    //   title: Text(
-    //     args.name,
-    //     style: _biggerFont,
-    //   ),
-    //   trailing: Icon(Icons.delete),
-    //   onTap: () {
-    //     setState(() {
-    //       null;
-    //     });
-    //   },
-    // );
+          );
+        });
   }
 
   @override
